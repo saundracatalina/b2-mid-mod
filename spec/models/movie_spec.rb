6fe3ea22a8dbbe76ec22cfee57d35a905bc2e764
Saundra <saundracatalina@gmail.com>
2021-01-04 10:43:28 -0700

    Add tests for many-to-many relationship through joins table

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Movie, type: :model do
   describe 'relationship' do
     it { should belong_to :studio }
+    it { should have_many(:actors).through(:movie_actors) }
   end
 end
